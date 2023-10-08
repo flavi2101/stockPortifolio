73e881e9345a5b18aac50e8f0f7548ee913ed108
Flavio Henrique <flavi2101@gmail.com>
2023-10-08 10:57:28 -0300

    menu filter role

--- app/components/server/menu.js
+++ app/components/server/menu.js
@@ -1,40 +1,9 @@
-import Link from "next/link";
-import { authOptions } from "../../api/auth/[...nextauth]/route";
-import { getServerSession } from "next-auth";
-
+import { MenuItens } from "./menuItens";
 async function Menu() {
-
-  const session = await getServerSession(authOptions)
-  
-  let nav = [
-    //TODO: alterar roda dinamida id
-    {
-      name: "Minha Carteira",
-      path:`/dashboard/user/${session.user.id}/meu_portifolio`,
-      
-    },
-    {
-      name: "Recomendaçoes",
-      path:`/dashboard/user/${session.user.id}/recomendacoes`,
-      
-    },
-    {
-      name: "Carteira Roberto",
-      path:`/dashboard/user/${session.user.id}/roberto_portifolio`,
-      
-    },
-   
-  ];
   return (
     <nav>
       <ul className="flex flex-row h-10 bg-blue-600  justify-around items-center">
-        {nav.map((item, index) => {
-          return (
-            <li key={index}>
-              <Link href={item.path}>{item.name}</Link>
-            </li>
-          );
-        })}
+        <MenuItens></MenuItens>
       </ul>
     </nav>
   );
---
+++ app/components/server/menuItens.js
@@ -0,0 +1,95 @@
+import Link from "next/link";
+import { authOptions } from "../../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+
+export async function MenuItens() {
+  const session = await getServerSession(authOptions);
+  const itens = [
+    {
+      name: "Minha Carteira",
+      path: `/dashboard/user/${session.user.id}/meu_portifolio`,
+    },
+    {
+      name: "Analises",
+      path: `/dashboard/user/${session.user.id}/analise`,
+    },
+    {
+      name: "Recomendaçoes",
+      path: `/dashboard/user/${session.user.id}/recomendacoes`,
+    },
+    {
+      name: "Carteira Roberto",
+      path: `/dashboard/user/${session.user.id}/roberto_portifolio`,
+    },
+    {
+      name: "Mentoria",
+      path: `https://escoladafortuna.com/valueinvesting/`,
+    },
+    {
+      name: "Sobre",
+      path: `/dashboard/user/${session.user.id}/about`,
+    },
+    {
+      name: "Mentores",
+      path: `/dashboard/user/${session.user.id}/mentor`,
+    },
+    {
+      name: "admin",
+      path: `/dashboard/user/${session.user.id}/admin`,
+    },
+  ];
+  if (session.user.role === "VISITANTE") {
+    let menuVisitante = itens.filter((item) => {
+      if (
+        item.name !== "Analises" &&
+        item.name !== "Recomendaçoes" &&
+        item.name !== "Carteira Roberto" &&
+        item.name !== "Mentores" &&
+        item.name !== "admin"
+      ) {
+        return item;
+      }
+    });
+    return menuVisitante.map((item, index) => {
+      return (
+        <li key={index}>
+          <Link href={item.path}>{item.name}</Link>
+        </li>
+      );
+    });
+  } else if (session.user.role === "MENTORADO") {
+    let menuMentorado = itens.filter((item) => {
+      if (item.name !== "Mentores" && item.name !== "admin") {
+        return item;
+      }
+    });
+    return menuMentorado.map((item, index) => {
+      return (
+        <li key={index}>
+          <Link href={item.path}>{item.name}</Link>
+        </li>
+      );
+    });
+  } else if (session.user.role === "MENTOR") {
+    let menuMentor = itens.filter((item) => {
+      if (item.name !== "admin") {
+        return item;
+      }
+    });
+    return menuMentor.map((item, index) => {
+      return (
+        <li key={index}>
+          <Link href={item.path}>{item.name}</Link>
+        </li>
+      );
+    });
+  } else {
+    return itens.map((item, index) => {
+      return (
+        <li key={index}>
+          <Link href={item.path}>{item.name}</Link>
+        </li>
+      );
+    });
+  }
+}
